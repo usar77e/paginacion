bbd29c9cbb7a02114da67dc467ad8c7ec64fc6d8
usar77 <agenda212013@gmail.com>
2021-12-27 16:43:22 -0300

    front end ok
    cambios finales ok

--- frontend/src/app/paises/paises.component.ts
+++ frontend/src/app/paises/paises.component.ts
@@ -8,10 +8,15 @@ import { PaisesService } from '../paises.service';
 })
 export class PaisesComponent implements OnInit {
 
+  paises: Array<any>;
+  totalPages: Array<number>;
+
   page = 0;
   size = 4;
   order = 'id';
   asc = true;
+  isFirst = false;
+  isLast = false;
 
   constructor(private paisesService: PaisesService) { }
 
@@ -22,13 +27,45 @@ export class PaisesComponent implements OnInit {
   cargarPaises(){
     this.paisesService.paises(this.page, this.size, this.order, this.asc).subscribe(
       data => {
+        this.paises = data.content;
+        this.isFirst = data.first;
+        this.isLast = data.last;
+        this.totalPages = new Array(data['totalPages']);
         console.log(data);
       },
       err => {
         console.log(err.error);
-        )
+        
       }
     );
   }
 
+  sort(): void {
+    this.asc = !this.asc;
+    this.cargarPaises();
+  }
+
+  rewind(): void{
+    if(!this.isFirst){
+      this.page--;
+      this.cargarPaises();
+    }
+  }
+
+  forward(): void{
+    if(!this.isLast){
+      this.page++;
+      this.cargarPaises();
+    }
+  }
+
+  setPage(page: number):void{
+    this.page = page;
+    this.cargarPaises();
+  }
+
+  setOrder(order: string){
+    this.order = order;
+    this.cargarPaises();
+  }
 }
